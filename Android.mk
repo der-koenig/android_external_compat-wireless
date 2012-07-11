ce336033fb6546b4e499aef945becad27bb8781b
Ray Chen <raychen@qca.qualcomm.com>
2012-07-11 15:38:53 +0800

    compat-wireless: add ath6kl_sdio build support for OLCA 3.5
    Signed-off-by: Ray Chen <raychen@qca.qualcomm.com>

--- Android.mk
+++ Android.mk
@@ -8,34 +8,15 @@ export HAVE_CFG80211=1
 
 include $(CLEAR_VARS)
 LOCAL_MODULE             := cfg80211.ko
-LOCAL_MODULE_TAGS        := eng
+LOCAL_MODULE_TAGS        := debug
 LOCAL_MODULE_PATH        := $(TARGET_OUT)/lib/modules/ath6kl
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
 include $(CLEAR_VARS)
 LOCAL_MODULE             := ath6kl_sdio.ko
 LOCAL_MODULE_KBUILD_NAME := wlan.ko
-LOCAL_MODULE_TAGS        := eng
+LOCAL_MODULE_TAGS        := debug
 LOCAL_MODULE_PATH        := $(TARGET_OUT)/lib/modules/ath6kl
-include $(DLKM_DIR)/AndroidKernelModule.mk
-endif
-
-ifeq ($(BOARD_HAS_ATH_WLAN_AR6004), true)
-
-export BUILD_ATH6KL_VER_35=1
-export HAVE_CFG80211=1
-
-include $(CLEAR_VARS)
-LOCAL_MODULE             := cfg80211.ko
-LOCAL_MODULE_TAGS        := eng
-LOCAL_MODULE_PATH        := $(TARGET_OUT)/lib/modules/ath6kl-3.5
-include $(DLKM_DIR)/AndroidKernelModule.mk
-
-include $(CLEAR_VARS)
-LOCAL_MODULE             := ath6kl_usb.ko
-LOCAL_MODULE_TAGS        := eng
-LOCAL_MODULE_KBUILD_NAME := wlan.ko
-LOCAL_MODULE_PATH        := $(TARGET_OUT)/lib/modules/ath6kl-3.5
 include $(DLKM_DIR)/AndroidKernelModule.mk
 endif
 
@@ -45,15 +26,29 @@ export BUILD_ATH_ETH_ALX=1
 
 include $(CLEAR_VARS)
 LOCAL_MODULE             := compat.ko
-LOCAL_MODULE_TAGS        := eng
+LOCAL_MODULE_TAGS        := debug
 LOCAL_MODULE_PATH        := $(TARGET_OUT)/lib/modules/compat
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
 include $(CLEAR_VARS)
 LOCAL_MODULE             := alx.ko
 LOCAL_MODULE_KBUILD_NAME := eth.ko
-LOCAL_MODULE_TAGS        := eng
+LOCAL_MODULE_TAGS        := debug
 LOCAL_MODULE_PATH        := $(TARGET_OUT)/lib/modules/alx
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
+endif
+
+ifeq ($(BOARD_HAS_ATH_WLAN_AR6004), true)
+
+export BUILD_ATH6KL_VER_35=1
+export HAVE_CFG80211=1
+
+include $(CLEAR_VARS)
+LOCAL_MODULE             := cfg80211.ko
+LOCAL_MODULE_TAGS        := debug
+LOCAL_MODULE_PATH        := $(TARGET_OUT)/lib/modules/ath6kl-3.5
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(LOCAL_PATH)/drivers/net/wireless/ath/ath6kl-3.5/Android.mk
 endif
---
+++ drivers/net/wireless/ath/ath6kl-3.5/Android.mk
@@ -0,0 +1,5 @@
+ifeq ($(BOARD_HAS_ATH_WLAN_AR6004), true)
+
+include $(call all-subdir-makefiles)
+
+endif
