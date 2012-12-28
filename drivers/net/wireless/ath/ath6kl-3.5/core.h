4db4eeea427da5499d23494d27298c52268e00f5
Ray Chen <raychen@qca.qualcomm.com>
2012-12-28 12:12:40 +0800

    compat-wireless: Update OLCA 3.5 to 3.5.0.237

--- drivers/net/wireless/ath/ath6kl-3.5/core.h
+++ drivers/net/wireless/ath/ath6kl-3.5/core.h
@@ -46,7 +46,7 @@
 #define TO_STR(symbol) MAKE_STR(symbol)
 
 /* The script (used for release builds) modifies the following line. */
-#define __BUILD_VERSION_ (3.5.0.236)
+#define __BUILD_VERSION_ (3.5.0.237)
 
 #define DRV_VERSION		TO_STR(__BUILD_VERSION_)
 
